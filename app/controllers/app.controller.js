8c340f5f993f1c694e4db0af0f9cb8c77a7a99c6
Osman Pérez <operez@edutecno.com>
2023-07-27 21:48:10 -0400

    Consultas con relaciones

--- app/controllers/app.controller.js
+++ app/controllers/app.controller.js
@@ -10,3 +10,39 @@ exports.createUser = (user) => {
         console.log("Error registrando usuario", err);
     })
 }
+
+exports.createProject = (userId, project) => {
+    return projects.create({ ...project, userId: userId}).then(response => {
+        console.log("proyecto creado", JSON.stringify(response, null, 4));
+        return response
+    })
+}
+
+exports.consultarUsuario = (userId) => {
+    return users.findByPk(userId, { include: ["projects"]})
+                .then(response => {
+                    return response
+                })
+                .catch(err => {
+                    console.log("Error consultando Usaurio", err);
+                })
+} 
+
+exports.consultarProyecto = (projectId) => {
+    return projects.findByPk(projectId, { include: ["user"]})
+                .then(response => {
+                    return response
+                }).catch(err => {
+                    console.log("Error consultando proyecto", err);
+                })
+}
+
+exports.listarUsuarios = () => {
+    return users.findAll({ include: ["projects"]})
+                .then(response => {
+                    return response
+                })
+                .catch(err => {
+                    console.log("Error en listado de usuarios", err);
+                })
+}
--- server.js
+++ server.js
@@ -2,7 +2,23 @@ const db = require("./app/models/index")
 const controller = require("./app/controllers/app.controller")
 
 const main = async() => {
-    const user1 = await controller.createUser({ name: "operez"})
+    // const user1 = await controller.createUser({ name: "operez"})
+    // const user2 = await controller.createUser({ name: "lmartinez"})
+    // const user3 = await controller.createUser({ name: "palvarez"})
+
+    // const project1 = await controller.createProject(1, { name: "Proyecto 1"})
+    // const project2 = await controller.createProject(2, { name: "Proyecto 2"})
+    // const project3 = await controller.createProject(2, { name: "Proyecto 3"})
+
+
+    const datosUsuario = await controller.consultarUsuario(2)
+    console.log("Datos de usuario", JSON.stringify(datosUsuario, null,4));
+
+    const datosProyecto = await controller.consultarProyecto(1)
+    console.log("Datos de proyecto", JSON.stringify(datosProyecto, null, 4));
+
+    const listaUsuarios = await controller.listarUsuarios()
+    console.log("Listado de Usuarios", JSON.stringify(listaUsuarios, null,4));
 
     console.log("Ejecución exitosa");
 }
